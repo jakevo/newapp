ad27aa0757b2b60244501c83f28b6be8ac5a4c49
Jake Vo <javo@ucsd.edu>
2016-11-28 23:30:03 -0800

    add myproject

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 var html_dir = './html/';
 var connect = "postgres://wkffstrhlpupdr:DVb427-jc3Z7gnunzpVeA6XFji@ec2-54-221-244-62.compute-1.amazonaws.com:5432/d3vt508c43cdvj?ssl=true"
 var name;
-
+var projectID;
 //var pool = new pg.Pool(config);&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory
 app.engine('dust', cons.dust);
 
@@ -70,7 +70,6 @@ app.get('/signin', function(req, res) {
 
 
 
-
 app.get('/signup', function(req, res) {
 
   res.render('signup',{user: name});
@@ -117,11 +116,12 @@ app.get('/forgotpassword', function(req, res) {
 
 });
 
+/*
 app.get('/myprojects', function(req, res) {
 
   res.render('myprojects',{user: name});
 
-});
+});*/
 
 app.get('/analyze', function(req, res) {
 
@@ -130,7 +130,28 @@ app.get('/analyze', function(req, res) {
 
 app.get('/data', function(req, res) {
 
-  res.render('data');
+  res.render('data', {user: name});
+
+});
+
+app.get('/myprojects', function(req, res) {
+
+  pg.connect(connect, function (err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+
+    client.query("SELECT * FROM question where owner = $1",[name], function(err, result) {
+
+      if (err) {
+
+           return res.render('myprojects', {projects: ""});
+      }
+
+      res.render('myprojects', {projects: result.rows});
+      done();
+    });
+  });
 
 });
 
@@ -149,10 +170,6 @@ app.post('/addUser', function(req, res) {
       //console.log(result.rows);
       //res.render('index');
       for (var i = 0; i < result.rows.length; i++) {
-        console.log("database " + result.rows[i].email);
-        console.log("database " + result.rows[i].password);
-        console.log(req.body.logU);
-        console.log(req.body.logP);
         if (result.rows[i].email == req.body.logU && result.rows[i].password == req.body.logP) {
           name = result.rows[i].email;
           return res.render('home', {user: result.rows[i].email});
@@ -211,8 +228,9 @@ app.post('/display', function(req, res) {
     if(err) {
       return console.error('error fetching client from pool', err);
     }
-    console.log(req.body.projectID);
+
     client.query("SELECT * FROM question where projname = $1",[req.body.projectID], function(err, result) {
+      projectID = req.body.projectID;
       res.render('project', {info: result.rows, user: name});
       done();
     });
